82ba6fb79f828c23f387a917db40f69551ab02c6
PedroDiSanti <pedroa.disanti@gmail.com>
2024-04-12 18:00:52 -0300

    refactor: changing solution file name and adding .idea on .gitignore

--- 88_merge_sorted_array/merge_sorted_array.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-class Solution:
-    @staticmethod
-    def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        del (nums1[m:(m + n)])
-        nums1.extend(nums2)
-        nums1.sort()
